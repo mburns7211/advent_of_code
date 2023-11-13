8ce55fd14efb0e0e827f0f875c74b6a7c0bc6def
Matt Burns <matt@DESKTOP-62LNCEI.localdomain>
2023-11-13 11:57:37 -0500

    Day 1 complete, part 1 and 2

---
+++ 2022/day_1_pt2.py
@@ -0,0 +1,19 @@
+path = "inputs/day1_input.txt"
+f = open(path, "r")
+
+input_lst = f.read().split("\n")
+
+
+curr_sum = 0
+sum_lst = []
+
+for i in input_lst:
+    if i == "":
+        sum_lst.append(curr_sum)
+        curr_sum = 0
+    else:
+        curr_sum += int(i)
+
+sum_lst.sort()
+top_three = sum_lst[-1] + sum_lst[-2] + sum_lst[-3]
+print(f"top 3 sum: {top_three}")
